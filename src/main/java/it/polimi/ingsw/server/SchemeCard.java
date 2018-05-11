4b033aac11d933b7ee310e718f962373255c5c40
Tallino <gdanese@hotmail.it>
2018-05-11 10:59:43 +0200

    SchemeCard methods added

--- src/main/java/it/polimi/ingsw/server/SchemeCard.java
+++ src/main/java/it/polimi/ingsw/server/SchemeCard.java
@@ -9,65 +9,159 @@ public class SchemeCard
     private String backName;
     private int frontDiff;
     private int backDiff;
+    private int fb;
 
-    private int back[][];
     private int front[][];
+    private int back[][];
+
     private Die diceScheme[][];
-    public SchemeCard()
+
+    public SchemeCard(int i)
     {
+        fb=0;
+        id=i;
+        frontName="";
+        backName="";
+        frontDiff=0;
+        backDiff=0;
+        front = new int[4][5];
+        back = new int[4][5];
 
+        for(int j=0;j<4;j++)
+            for(int z=0;z<5;z++)
+            {
+                front[j][z]=0;
+                back[j][z]=0;
+            }
+
+        for(int j=0;j<4;j++)
+            for(int z=0;z<5;z++)
+            {
+                diceScheme[j][z]=null;
+            }
     }
-
 
     public int[][] getFront()
     {
-        // ritorna lo schema fronte
-        return null;
+        int[][] temp = new int[4][5];
+        for(int i=0;i<4;i++)
+            for(int j=0;j<5;j++)
+            {
+                temp[i][j] = front[i][j];
+            }
+        return temp;
+
     }
 
     public int[][] getBack()
     {
-        // ritorna lo schema retro
-        return null;
+        int[][] temp = new int[4][5];
+        for(int i=0;i<4;i++)
+            for(int j=0;j<5;j++)
+            {
+                temp[i][j] = back[i][j];
+            }
+        return temp;
+    }
+
+    public void setfb(int fb)
+    {
+        if (fb==1)
+            this.fb=1;
+
+        if (fb==2)
+            this.fb=2;
 
     }
 
+    public int getfb()
+    {
+
+        return fb;
+
+    }
+
+
     public void setCell(int fb, int x, int y, int toInsert)
     {
-        // imposta il contenuto della cella x,y
+        //fronte: fr=1 / retro: fr=2
+
+        if(fb==1)
+            front[x][y]=toInsert;
+        if(fb==2)
+            back[x][y]=toInsert;
+
     }
 
     public int getCell(int fb, int x, int y)
     {
-        // ritorna il contenuto della cella x,y
-        return 0;
+        //fronte: fr=1 / retro: fr=2
+
+        int temp=0;
+
+        if(fb==1)
+            temp = front[x][y];
+
+        if(fb==2)
+            temp = back[x][y];
+
+        return temp;
+
     }
 
-    public void getID()
+    public int getID()
     {
-        // ritorna l'id
+        return id;
     }
 
     public void setDiff(int fb, int diff)
     {
-        // imposta la difficoltà dello schema
+        //fronte: fr=1 / retro: fr=2
+
+        if (fb==1)
+            this.frontDiff = diff;
+        if (fb==2)
+            this.backDiff = diff;
     }
 
-    public int getDiff(int fb, int diff)
-    {
-        // ritorna la difficoltà
-        return 0;
+    public int getDiff(int fb) {
+        //fronte: fr=1 / retro: fr=2
+
+        if (fb == 1) {
+            return this.frontDiff;
+        }
+
+        if (fb == 2) {
+            return backDiff;
+        }
+
     }
 
-    public Die getDie(int fb, int x, int y)
+
+
+    public Die getDie(int x, int y)
     {
         // ritorna il dado in posizione x,y
-        return null;
+        return diceScheme[x][y];
+
     }
+
 
     public void setDie(Die toPlace, int x, int y)
     {
+
         // posiziona il dado passato come parametro in posizione x,y
+
+        if (toPlace!=null) {
+            if (x > 0 && x < 4) {
+                if (y > 0 && y < 5) {
+                    if (diceScheme[x][y]==null)
+                        diceScheme[x][y] = toPlace;
+
+                }
+
+            }
+        }
     }
 
 }
