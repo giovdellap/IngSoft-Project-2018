a46b1cb56d2b4bfb5c5359a425e6988eda889b3d
giovdellap <giovanninicola.dellapelle@mail.polimi.it>
2018-05-15 12:16:56 +0200

    final

--- src/main/java/it/polimi/ingsw/server/PublicObjective.java
+++ src/main/java/it/polimi/ingsw/server/PublicObjective.java
@@ -1,23 +1,26 @@
 package it.polimi.ingsw.server;
 
+import it.polimi.ingsw.server.ServerExceptions.GenericInvalidArgumentException;
 import it.polimi.ingsw.server.ServerExceptions.InvalidIntArgumentException;
 
 import java.util.*;
 
-public class PublicObjective
-{
+public class PublicObjective {
+    //COLORS 0 WHITE/1 YELLOW/2 RED/3 GREEN/4 BLUE/5 VIOLET
+    //NUMERI 6 ONE/7 TWO/8 THREE/9 FOUR/10 FIVE/11 SIX
+
     private int id;
     private int bonus;
-    private boolean isDisabled= false;
+    private boolean isDisabled = false;
 
-    public PublicObjective(int id) throws InvalidIntArgumentException
-    {
 
-        if (id<0 || id>10)
+    public PublicObjective(int id) throws InvalidIntArgumentException {
+
+        if (id < 0 || id > 10)
             throw new InvalidIntArgumentException();
 
-        this.id=id;
-        this.bonus=0;
+        this.id = id;
+        this.bonus = 0;
 
     }
 
@@ -35,7 +38,7 @@ public class PublicObjective
     public void disableScheme() {
 
         if (!isDisabled)
-            isDisabled=true;
+            isDisabled = true;
 
     }
 
@@ -48,44 +51,44 @@ public class PublicObjective
 
     public int setBonus(SchemeCard scheme) throws InvalidIntArgumentException {
 
-        if (id==1) {
+        if (id == 1) {
             bonus = calculateOne(scheme);
         }
 
-        if (id==2) {
-            this.bonus = 5;
+        if (id == 2) {
+            bonus = calculateTwo(scheme);
         }
 
-        if (id==3) {
-            this.bonus = 5;
+        if (id == 3) {
+            bonus = calculateThree(scheme);
         }
 
-        if (id==4) {
-            this.bonus = 4;
+        if (id == 4) {
+            bonus = calculateFour(scheme);
         }
 
-        if (id==5) {
-            this.bonus = 2;
+        if (id == 5) {
+            bonus = calculateFive(scheme);
         }
 
-        if (id==6) {
-            this.bonus = 2;
+        if (id == 6) {
+             bonus = calculateSix(scheme);
         }
 
-        if (id==7) {
-            this.bonus = 2;
+        if (id == 7) {
+            bonus = calculateSeven(scheme);
         }
 
-        if (id==8) {
-            this.bonus = 5;
+        if (id == 8) {
+            bonus = calculateEight(scheme);
         }
 
-        if (id==9) {
-            this.bonus = 0;   // # TO DO
+        if (id == 9) {
+            this.bonus=9;
         }
 
-        if (id==10) {
-            this.bonus = 4;
+        if (id == 10) {
+            bonus = calculateTen(scheme);
         }
 
         return bonus;
@@ -102,7 +105,7 @@ public class PublicObjective
 
         return list;
 
-        }
+    }
 
 
     private int calculateOne(SchemeCard scheme) throws InvalidIntArgumentException {
@@ -111,87 +114,280 @@ public class PublicObjective
         int bonus = 0;
         int[] temp = new int[5];
 
-        for(int i=0;i<4;i++)
-        {
-            for(int j=0;j<5;j++)
-            {
-                temp[j]=1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                temp[j] = 1;
             }
 
-            boolean flag=true;
+            boolean flag = true;
 
-            for(int k=0;k<5;k++)
-            {
-                if(temp[scheme.getDie(i,k).getColor()-1]==0)
+            for (int k = 0; k < 5; k++) {
+                if (scheme.getDie(i, k).getColor() != 0) {
+                    if (temp[scheme.getDie(i, k).getColor() - 1] == 0)
+                        flag = false;
+
+                    temp[scheme.getDie(i, k).getColor() - 1] = 0;
+                }
+
+                if (scheme.getDie(i, k).getColor() == 0)
                     flag = false;
 
-                temp[scheme.getDie(i,k).getColor()-1]=0;
             }
 
-            if(flag)
-                bonus=bonus+6;
+            if (flag)
+                bonus = bonus + 6;
 
         }
 
         return bonus;
     }
+
 
     private int calculateTwo(SchemeCard scheme) throws InvalidIntArgumentException {
-        int bonus=0;
+
+        int bonus = 0;
         int[] temp = new int[5];
-        for(int i=0;i<5;i++)
-        {
-            for(int j=0;j<4;j++)
-                temp[j]=1;
-            for(int z=0;z<4;z++)
-                temp[scheme.getDie(i,z).getColor()-1]=0;
-            int check=2;
-            for(int j=0;j<4;j++)
-                if(temp[j]==1)
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++)
+                temp[j] = 1;
+            boolean flag = true;
+            for (int z = 0; z < 4; z++) {
+
+                if (scheme.getDie(z, i).getColor() != 0)
+                    temp[scheme.getDie(z, i).getColor() - 1] = 0;
+                if (scheme.getDie(z, i).getColor() == 0)
+                    flag = false;
+            }
+            int check = 2;
+
+            for (int j = 0; j < 5; j++)
+                if (temp[j] == 1)
                     check--;
-            if(check==1)
-                bonus=bonus+5;
+
+            if (check == 1 && flag)
+                bonus = bonus + 5;
+
         }
+
         return bonus;
     }
 
+
     private int calculateThree(SchemeCard scheme) throws InvalidIntArgumentException {
-        int bonus=0;
+        int bonus = 0;
         int[] temp = new int[6];
-        for(int i=0;i<4;i++)
-        {
-            for(int j=0;j<6;j++)
-                temp[j]=1;
-            for(int j=0;j<5;j++)
-                temp[scheme.getDie(i,j).getValue()-1]=0;
-            int test=2;
-            for(int j=0;j<6;j++)
-                if(temp[j]==1)
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 6; j++)
+                temp[j] = 1;
+            for (int j = 0; j < 5; j++)
+                if (scheme.getDie(i, j).getColor() != 0)
+                    temp[scheme.getDie(i, j).getValue() - 1] = 0;
+            int test = 2;
+            for (int j = 0; j < 6; j++)
+                if (temp[j] == 1)
                     test--;
-            if(test==1)
-                bonus=bonus+5;
+            if (test == 1)
+                bonus = bonus + 5;
         }
         return bonus;
     }
 
     private int calculateFour(SchemeCard scheme) throws InvalidIntArgumentException {
-        int bonus=0;
+        int bonus = 0;
         int[] temp = new int[6];
-        for(int i=0;i<5;i++)
-        {
-            for(int j=0;j<6;j++)
-                temp[j]=1;
-            for(int j=0;j<4;j++)
-                temp[scheme.getDie(j,i).getValue()-1]=0;
-            int test=3;
-            for(int j=0;j<6;j++)
-                if(temp[j]==1)
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++)
+                temp[j] = 1;
+            for (int j = 0; j < 4; j++)
+                if (scheme.getDie(j, i).getColor() != 0)
+                    temp[scheme.getDie(j, i).getValue() - 1] = 0;
+
+            int test = 3;
+            for (int j = 0; j < 6; j++)
+                if (temp[j] == 1)
                     test--;
-            if(test==1)
-                bonus=bonus+4;
+            if (test == 1)
+                bonus = bonus + 4;
         }
         return bonus;
     }
+
+
+    private int calculateFive(SchemeCard scheme) throws InvalidIntArgumentException {
+        int bonus = 0;
+        int cont1 = 0;
+        int cont2 = 0;
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 0; j < 5; j++) {
+
+                if (scheme.getDie(i, j).getValue() == 1)
+                    cont1++;
+                if (scheme.getDie(i, j).getValue() == 2)
+                    cont2++;
+
+            }
+        }
+
+        if (cont1 < cont2) {
+            bonus = cont1 * 2;
+            return bonus;
+        }
+
+        if (cont2 < cont1) {
+            bonus = cont2 * 2;
+            return bonus;
+        }
+
+        else {
+            bonus = cont1 * 2;
+            return bonus;
+        }
+
+    }
+
+
+    private int calculateSix(SchemeCard scheme) throws InvalidIntArgumentException {
+
+        int bonus = 0;
+        int cont3 = 0;
+        int cont4 = 0;
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 0; j < 5; j++) {
+
+                if (scheme.getDie(i, j).getValue() == 3)
+                    cont3++;
+                if (scheme.getDie(i, j).getValue() == 4)
+                    cont4++;
+
+            }
+        }
+
+        if (cont3 < cont4) {
+            bonus = cont3 * 2;
+            return bonus;
+        }
+
+        if (cont4 < cont3) {
+            bonus = cont4 * 2;
+            return bonus;
+        }
+
+        else {
+            bonus = cont3 * 2;
+            return bonus;
+        }
+
+    }
+
+
+    private int calculateSeven(SchemeCard scheme) throws InvalidIntArgumentException {
+
+        int bonus = 0;
+        int cont5 = 0;
+        int cont6 = 0;
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 0; j < 5; j++) {
+
+                if (scheme.getDie(i, j).getValue() == 5)
+                    cont5++;
+                if (scheme.getDie(i, j).getValue() == 6)
+                    cont6++;
+
+            }
+        }
+
+        if (cont5 < cont6) {
+            bonus = cont5 * 2;
+            return bonus;
+        }
+
+        if (cont6 < cont5) {
+            bonus = cont6 * 2;
+            return bonus;
+        }
+
+        else {
+            bonus = cont5 * 2;
+            return bonus;
+        }
+
+    }
+
+
+    private int calculateEight(SchemeCard scheme) throws InvalidIntArgumentException {
+        int bonus = 0;
+        int min = 3;
+        int[] temp = new int[6];
+
+        for (int i=0;i<6;i++)
+            temp[i]=0;
+
+        for (int i=0;i<4;i++) {
+            for(int j=0;j<5;j++) {
+                if (scheme.getDie(i, j).getValue() != 0)
+                    temp[scheme.getDie(i,j).getValue()-1]+=1;
+            }
+        }
+
+        for(int z=0;z<6;z++) {
+            if (temp[z]<=min)
+                min = temp[z];
+        }
+
+        if (min==0)
+            return bonus;
+
+        else {
+            bonus = 5 * min;
+            return bonus;
+        }
+    }
+
+
+    private int calculateTen(SchemeCard scheme) throws InvalidIntArgumentException {
+        int bonus = 0;
+        int min = 4;
+        int[] temp = new int[5];
+
+        for (int i=0;i<5;i++)
+            temp[i]=0;
+
+        for (int i=0;i<4;i++) {
+            for(int j=0;j<5;j++) {
+                if (scheme.getDie(i, j).getColor() != 0)
+                    temp[scheme.getDie(i,j).getColor()-1]+=1;
+            }
+        }
+
+        for(int z=0;z<5;z++) {
+            if (temp[z]<=min)
+                min = temp[z];
+        }
+
+        if (min==0)
+            return bonus;
+
+        else {
+            bonus = 4 * min;
+            return bonus;
+        }
+
+
+
+
+    }
+
+
+
 }
+
 
 
---
+++ src/test/java/it/polimi/ingsw/server/PublicObjectiveCalculationsTest.java
@@ -0,0 +1,313 @@
+package it.polimi.ingsw.server;
+
+import it.polimi.ingsw.server.ServerExceptions.GenericInvalidArgumentException;
+import it.polimi.ingsw.server.ServerExceptions.InvalidIntArgumentException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PublicObjectiveCalculationsTest {
+
+    //COLORS 0 WHITE/1 YELLOW/2 RED/3 GREEN/4 BLUE/5 VIOLET
+    //NUMERI 6 ONE/7 TWO/8 THREE/9 FOUR/10 FIVE/11 SIX
+
+    SchemesDeck schemesDeckTest;
+    SchemeCard[] schemesTest;
+    SchemeCard schemeTest;
+    PublicObjective publicObjectiveTest;
+    int bonusTest;
+    int[][] diceSchemeTest;
+    Die DieTest1;
+    Die DieTest2;
+    Die DieTest3;
+    Die DieTest4;
+    Die DieTest5;
+    Die DieTest6;
+
+
+    @BeforeEach
+    public void setUp() throws InvalidIntArgumentException {
+
+        schemesDeckTest = new SchemesDeck();
+        schemesTest = schemesDeckTest.extractSchemes(2);
+        schemeTest = schemesTest[1];
+
+    }
+
+
+    @Test
+    public void checkCalculateOne() throws GenericInvalidArgumentException, InvalidIntArgumentException {
+
+        publicObjectiveTest = new PublicObjective(1);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+        DieTest5 = new Die(5);
+
+        schemeTest.setDie(DieTest1,1,0);
+        schemeTest.setDie(DieTest2,1,1);
+        schemeTest.setDie(DieTest3,1,2);
+        schemeTest.setDie(DieTest4,1,3);
+        schemeTest.setDie(DieTest5,1,4);
+
+        schemeTest.setDie(DieTest1,3,0);
+        schemeTest.setDie(DieTest2,3,1);
+        schemeTest.setDie(DieTest3,3,2);
+        schemeTest.setDie(DieTest4,3,3);
+        schemeTest.setDie(DieTest5,3,4);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(12,bonusTest);
+
+
+    }
+
+    @Test
+    public void checkCalculateTwo() throws GenericInvalidArgumentException, InvalidIntArgumentException {
+
+        publicObjectiveTest = new PublicObjective(2);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+
+        schemeTest.setDie(DieTest1,0,1);
+        schemeTest.setDie(DieTest2,1,1);
+        schemeTest.setDie(DieTest3,2,1);
+        schemeTest.setDie(DieTest4,3,1);
+
+        schemeTest.setDie(DieTest1,0,4);
+        schemeTest.setDie(DieTest2,1,4);
+        schemeTest.setDie(DieTest3,2,4);
+        schemeTest.setDie(DieTest4,3,4);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(10,bonusTest);
+
+
+    }
+
+
+    @Test
+    public void checkCalculateThree() throws GenericInvalidArgumentException, InvalidIntArgumentException {
+
+        publicObjectiveTest = new PublicObjective(3);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+        DieTest5 = new Die(5);
+
+
+        DieTest1.setValueTest(1);
+        DieTest2.setValueTest(3);
+        DieTest3.setValueTest(2);
+        DieTest4.setValueTest(6);
+        DieTest5.setValueTest(5);
+
+        schemeTest.setDie(DieTest1,1,0);
+        schemeTest.setDie(DieTest2,1,1);
+        schemeTest.setDie(DieTest3,1,2);
+        schemeTest.setDie(DieTest4,1,3);
+        schemeTest.setDie(DieTest5,1,4);
+
+        schemeTest.setDie(DieTest1,3,0);
+        schemeTest.setDie(DieTest2,3,1);
+        schemeTest.setDie(DieTest3,3,2);
+        schemeTest.setDie(DieTest4,3,3);
+        schemeTest.setDie(DieTest5,3,4);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(10,bonusTest);
+
+
+    }
+
+    @Test
+    public void checkCalculateFour() throws GenericInvalidArgumentException, InvalidIntArgumentException {
+
+        publicObjectiveTest = new PublicObjective(4);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+
+        DieTest1.setValueTest(1);
+        DieTest2.setValueTest(3);
+        DieTest3.setValueTest(2);
+        DieTest4.setValueTest(6);
+
+        schemeTest.setDie(DieTest1,0,1);
+        schemeTest.setDie(DieTest2,1,1);
+        schemeTest.setDie(DieTest3,2,1);
+        schemeTest.setDie(DieTest4,3,1);
+
+        schemeTest.setDie(DieTest1,0,4);
+        schemeTest.setDie(DieTest2,1,4);
+        schemeTest.setDie(DieTest3,2,4);
+        schemeTest.setDie(DieTest4,3,4);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(8,bonusTest);
+
+
+    }
+
+
+    @Test
+    public void checkCalculateFive() throws InvalidIntArgumentException, GenericInvalidArgumentException {
+
+        publicObjectiveTest = new PublicObjective(5);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+        DieTest5 = new Die(4);
+
+        DieTest1.setValueTest(1);
+        DieTest2.setValueTest(1);
+        DieTest3.setValueTest(2);
+        DieTest4.setValueTest(2);
+        DieTest5.setValueTest(5);
+
+        schemeTest.setDie(DieTest1,0,1);
+        schemeTest.setDie(DieTest2,3,3);
+        schemeTest.setDie(DieTest3,1,4);
+        schemeTest.setDie(DieTest4,2,4);
+        schemeTest.setDie(DieTest5,1,2);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(4,bonusTest);
+
+
+    }
+
+    @Test
+    public void checkCalculateSix() throws InvalidIntArgumentException, GenericInvalidArgumentException {
+
+        publicObjectiveTest = new PublicObjective(6);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+        DieTest5 = new Die(4);
+
+        DieTest1.setValueTest(3);
+        DieTest2.setValueTest(2);
+        DieTest3.setValueTest(3);
+        DieTest4.setValueTest(4);
+        DieTest5.setValueTest(4);
+
+        schemeTest.setDie(DieTest1,0,1);
+        schemeTest.setDie(DieTest2,3,3);
+        schemeTest.setDie(DieTest3,1,4);
+        schemeTest.setDie(DieTest4,2,4);
+        schemeTest.setDie(DieTest5,1,2);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(4,bonusTest);
+
+
+    }
+
+    @Test
+    public void checkCalculateSeven() throws InvalidIntArgumentException, GenericInvalidArgumentException {
+
+        publicObjectiveTest = new PublicObjective(7);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+        DieTest5 = new Die(4);
+
+        DieTest1.setValueTest(5);
+        DieTest2.setValueTest(6);
+        DieTest3.setValueTest(6);
+        DieTest4.setValueTest(6);
+        DieTest5.setValueTest(4);
+
+        schemeTest.setDie(DieTest1,0,1);
+        schemeTest.setDie(DieTest2,3,3);
+        schemeTest.setDie(DieTest3,1,4);
+        schemeTest.setDie(DieTest4,2,4);
+        schemeTest.setDie(DieTest5,1,2);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(2,bonusTest);
+
+
+    }
+
+    @Test
+    public void checkCalculateEight() throws InvalidIntArgumentException, GenericInvalidArgumentException {
+
+        publicObjectiveTest = new PublicObjective(8);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+        DieTest5 = new Die(4);
+        DieTest6 = new Die(4);
+
+        DieTest1.setValueTest(1);
+        DieTest2.setValueTest(2);
+        DieTest3.setValueTest(3);
+        DieTest4.setValueTest(4);
+        DieTest5.setValueTest(5);
+        DieTest6.setValueTest(6);
+
+        schemeTest.setDie(DieTest1,0,1);
+        schemeTest.setDie(DieTest2,3,3);
+        schemeTest.setDie(DieTest3,1,4);
+        schemeTest.setDie(DieTest4,2,4);
+        schemeTest.setDie(DieTest5,1,2);
+        schemeTest.setDie(DieTest6,3,1);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(5,bonusTest);
+
+
+    }
+
+    @Test
+    public void checkCalculateTen() throws InvalidIntArgumentException, GenericInvalidArgumentException {
+
+        publicObjectiveTest = new PublicObjective(10);
+
+        DieTest1 = new Die(1);
+        DieTest2 = new Die(2);
+        DieTest3 = new Die(3);
+        DieTest4 = new Die(4);
+        DieTest5 = new Die(5);
+
+        schemeTest.setDie(DieTest1,0,1);
+        schemeTest.setDie(DieTest2,3,3);
+        schemeTest.setDie(DieTest3,1,4);
+        schemeTest.setDie(DieTest4,2,4);
+        schemeTest.setDie(DieTest5,1,2);
+
+        bonusTest = publicObjectiveTest.setBonus(schemeTest);
+
+        Assertions.assertEquals(4,bonusTest);
+
+
+    }
+
+}
