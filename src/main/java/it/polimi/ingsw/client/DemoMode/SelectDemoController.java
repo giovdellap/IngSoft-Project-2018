6857b51adafa800472a36fb1f47c264d85439aa7
giovdellap <giovanninicola.dellapelle@mail.polimi.it>
2018-06-01 18:30:39 +0200

    New classic alpha CLI

--- src/main/java/it/polimi/ingsw/client/DemoMode/SelectDemoController.java
+++
@@ -1,4 +0,0 @@
-package it.polimi.ingsw.client.DemoMode;
-
-public class SelectDemoController {
-}
--- src/main/java/it/polimi/ingsw/client/PackageMP/ModelComponentMP.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.ingsw.client.PackageMP;
-
-public interface ModelComponentMP
-{
-}
